9894bf3e8543314610ea52730a4dc09b106074c5
TaaviE <taavi.eomae+github@gmail.com>
2018-12-02 02:41:01 +0200

    Added information about Babel(Ex) bugs and how to work around them

--- views.py
+++ views.py
@@ -49,7 +49,12 @@ from flask_mail import Message
 # Translation
 # Try switching between babelex and babel if you are getting errors
 from flask_babel import gettext as _
+# from flask_babelex import Domain
 # from flask_babelex import gettext as _
+# domain = Domain(domain="messages")
+# There is a function call you have to comment out for translations to be loaded when using BabelEx
+# I really don't want to start monkeypatching Babel(Ex) to fix this
+
 from main import babel
 
 
@@ -144,6 +149,7 @@ def favicon():
 
 
 def index():
+    # domain = Domain(domain="messages") # Uncomment this when using babelex
     user_id = session["user_id"]
     username = get_person_name(user_id)
     no_shuffle = False
