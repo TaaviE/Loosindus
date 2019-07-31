b882abb771f62a690724e40b11e8906908311acf
TaaviE <taavi.eomae+github@gmail.com>
2019-07-31 15:25:50 +0300

    Updated translated and translatable strings

--- babelmanagement.sh
+++ babelmanagement.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # Extract translations to file
-pybabel extract -F translations/babel.cf -k lazy_gettext -o translations/messages.pot .
+pybabel extract -F translations/babel.cfg -k lazy_gettext -o translations/messages.pot .
 
 # Initialize a translation
-pybabel init -i translations/messages.pot -d translations -l ee
-
-# Compile translations
-pybabel compile -d translations
+#pybabel init -i translations/messages.pot -d translations -l ee
 
 # Update translations
 pybabel update -i translations/messages.pot -d translations
 
 # Update individual translations
-pybabel update -i translations/messages.pot -d translations -l en
+#pybabel update -i translations/messages.pot -d translations -l en
+
+# Compile translations
+pybabel compile -d translations
