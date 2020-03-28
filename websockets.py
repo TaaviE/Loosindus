e32ca320ee3e193036da45cf18f21fb5b0969916
TaaviE <taavi.eomae+github@gmail.com>
2020-03-28 20:40:24 +0200

    Improved requirements, first steps towards websocket support

--- websockets.py
+++ websockets.py
@@ -2,3 +2,15 @@
 """
 Contains websocket related code
 """
+import eventlet
+from flask import Flask
+from flask_socketio import SocketIO
+
+from config import Config
+
+eventlet.monkey_patch()
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+socketio = SocketIO(app, message_queue=Config.MESSAGE_QUEUE_URI)
